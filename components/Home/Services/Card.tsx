f8f6f6511ff026d5a8c8b85f3ed2cacfe6cdfa31
MikkieTee02 <mikaelakayz@gmail.com>
2025-09-25 11:24:06 +0200

    add hover shadow to service cards

--- components/Home/Services/Card.tsx
+++ components/Home/Services/Card.tsx
@@ -10,7 +10,7 @@ type Props ={
 
 const Card = ({description, icon, name}:Props) => {
   return (
-    <div className='bg-slate-800  border-slate-700 px-4  py-6 border-2 rounded-lg  hover:border-teal-400 transition-all duration-300 group'>
+    <div className='bg-slate-800  hover:shadow-lg  border-slate-700 px-4  py-6 border-2 rounded-lg  hover:border-teal-400 transition-all duration-300 group'>
       <div className=' text-gray-500 bg-slate-950 border-black border-2 group-hover:border-teal-500 rounded-full inline-flex p-4 items-center justify-center group-hover:text-teal-400'>{icon}</div>
       <h2 className='mt-6 text-lg md:text-xl font-semibold text-gray-200 group-hover:text-teal-400'>{name}</h2>
       <p className='mt-6 text-gray-400'>{description}</p>
